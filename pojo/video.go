2a9f7d0b57ca53962ccf927af3d7329ad4aa513e
MuXiang123 <347213872@qq.com>
2022-06-12 14:24:58 +0800

    修复bug

--- pojo/video.go
+++ pojo/video.go
@@ -11,6 +11,6 @@ type Video struct {
 	FavoriteCount uint64 `json:"favorite_count" gorm:"column:favorite_count;default:0"` //点赞数
 	CommentCount  uint64 `json:"comment_count" gorm:"column:comment_count;default:0"`   //评论数
 	Title         string `json:"title" gorm:"column:title"`
-	Status        uint64 `grom:"column:title"`                  //视频状态 分 审核中 正常 下架
-	CreatedAt     int64  `json:"created_at" gorm:"create_time"` //投稿时间
+	Status        uint64 `grom:"column:title"`                         //视频状态 分 审核中 正常 下架
+	CreatedAt     int64  `json:"created_at" gorm:"column:create_time"` //投稿时间
 }
