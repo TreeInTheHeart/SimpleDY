1b21e816b2806b18f83719a2dbcb5cab2e9b6b3e
MuXiang123 <347213872@qq.com>
2022-06-12 13:25:09 +0800

    修复路由错误

--- handler/handler.go
+++ handler/handler.go
@@ -10,12 +10,12 @@ func Handler() {
 	r := gin.Default()
 	r.MaxMultipartMemory = 10 << 20 //短视频大小最大不超过 10M
 	//authMiddleware := r.Group("/publish",middleware.JwtMiddleWare())
-	r.POST("/douyin/user/register", api.Register)
-	r.POST("/douyin/user/login", api.Login)
+	r.POST("/douyin/user/register/", api.Register)
+	r.POST("/douyin/user/login/", api.Login)
 	r.GET("/douyin/user/", middleware.GetJwtMiddleWare(), api.GetInfo)
 	//r.POST("favorite/action", api.FavoriteOp)
-	r.POST("/douyin/publish/action", middleware.PostJwtMiddleWare(), api.Publish)
+	r.POST("/douyin/publish/action/", middleware.PostJwtMiddleWare(), api.Publish)
 	r.GET("/douyin/publish/list/", middleware.GetJwtMiddleWare(), api.GetPublishListByAuthorId)
-	r.GET("/douyin/feed", api.Feed)
+	r.GET("/douyin/feed/", api.Feed)
 	r.Run()
 }
